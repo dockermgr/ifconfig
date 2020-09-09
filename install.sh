fec0126f3016418442d1937a0929be60439adfcc
casjay <casjay@yahoo.com>
2020-09-09 18:30:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ DATADIR="/srv/docker/$APPNAME"
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull mpolden/echoip && docker restart $APPNAME
+docker pull mpolden/echoip:latest && docker restart $APPNAME
 else
 docker run \
 --detach \
 --name ifconfig \
 -p 127.0.0.1:85:8080 \
 --restart=always \
-mpolden/echoip echoip -p -H X-Forwarded-For
+mpolden/echoip:latest echoip -p -H X-Forwarded-For
 fi
