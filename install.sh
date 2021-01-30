dd3756d07906d74f708a2d0322278751543cd906
Jason <jason@casjay.net>
2021-01-29 23:27:59 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 APPNAME="ifconfig"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull mpolden/echoip:latest && docker restart $APPNAME
+  docker pull mpolden/echoip:latest && docker restart "$APPNAME"
 else
-docker run \
---detach \
---name ifconfig \
--p 85:8080 \
---restart=always \
-mpolden/echoip:latest echoip -p -H X-Forwarded-For
+  docker run \
+    --detach \
+    --name ifconfig \
+    -p 85:8080 \
+    --restart=always \
+    mpolden/echoip:latest echoip -p -H X-Forwarded-For
 fi
